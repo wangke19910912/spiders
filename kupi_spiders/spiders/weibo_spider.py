6338f8c69677fe99720f7aacada0ca642721a057
troy <troy@troydeMacBook-Pro.local>
2018-07-12 08:57:39 +0800

    add commit
    Signed-off-by: troy <troy@troydeMacBook-Pro.local>

--- kupi_spiders/spiders/weibo_spider.py
+++ kupi_spiders/spiders/weibo_spider.py
@@ -2,12 +2,16 @@
 # -*- coding: UTF-8 -*-
 import scrapy
 import json
-
+import MySQLdb
+import urllib
+import urllib2
+import sys
 
 class WeiboSpider(scrapy.Spider):
     name = "weibo"
     allowed_domains = ["weibo.cn"]
     start_urls = []
+    headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'} 
 
     def __init__(self, url_file=None, *args, **kwargs):
         super(WeiboSpider, self).__init__(*args, **kwargs)
@@ -19,17 +23,49 @@ class WeiboSpider(scrapy.Spider):
 			
 
     def parse(self, response):
-        #filename = response.url.split("/")[-2]
- 	filename  = "/Users/troy/workspace/local-code/scrapy/kupi_spiders/weibo_data/data"
-        data = ""
+	db = MySQLdb.connect("39.104.94.203", "dboper", "DB2018kp!", "kp", charset='utf8' )
+	cursor = db.cursor()
 	json_data = json.loads(response.body)
         cards = json_data['data']['cards']
         for card in cards:
 	    if card['card_type'] == 9:
+		resultDic = {}
 		mblog = card['mblog']
-		blog_id = card['itemid']
- 		text  = mblog['text'].encode('utf8')
-		data = data + str(blog_id) +"	"+str(text)+"\n"
-        	print("结束1")
-        with open(filename, 'a') as f:
-            f.write(data)
+		user_blog_id = card['itemid']
+		blog_id = mblog['id']	
+ 		text  = mblog['text']
+		if u"全文" in text:
+		    request_url = 'https://m.weibo.cn/statuses/extend?id=' + str(blog_id)
+		    print request_url
+		    req = urllib2.Request(request_url,headers=self.headers)
+		    res = urllib2.urlopen(req).read()
+		    #type = sys.getfilesystemencoding()	
+		    #print (res.decode('utf8').encode(type))
+		    text = json.loads(res)['data']['longTextContent']
+
+		resultDic['type']=0
+		resultDic['text'] = text.replace("'","\\\\\'")
+
+		if 'pics' in mblog:
+		    resultDic['type']=1
+		    resultDic['pics']=mblog['pics']	
+		if 'page_info' in mblog:
+		    resultDic['type']=2
+		    resultDic['page_info']=mblog['page_info'] 
+
+		resultJson = json.dumps(resultDic,ensure_ascii=False)
+		
+		print user_blog_id
+		sql = "INSERT INTO crawl_origin_data(\
+       		       thirdid, content, source, status) \
+                       VALUES ('%s', '%s', '%d', '%d' )" % \
+                       (user_blog_id,resultJson, 1,0)
+		
+		try:
+		    cursor.execute(sql)
+		    db.commit()
+		except Exception, e:
+		    print "error,........."
+		    print e
+		    db.rollback()
+	db.close()
